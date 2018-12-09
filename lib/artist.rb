64a42555f9ed15f8db3b1ee6b68a7e0ae27bca0a
Meisuleen Kased <meisuleen@gmail.com>
2018-12-09 05:08:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,7 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
+
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -11,9 +13,10 @@ class Artist
   end
 
   def add_song(song)
-    song = Song.new(song)
+    # song = Song.new(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
 end
